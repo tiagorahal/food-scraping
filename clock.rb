fec626f2bffb550c6249d25ff6fc9bb29d76601c
tiagorahal <rahal.aires@gmail.com>
2023-05-13 16:51:05 -0300

    fix the clockowrk execution

--- clock.rb
+++ clock.rb
@@ -1,12 +1,15 @@
 require 'clockwork'
 require './config/boot'
 require './config/environment'
+require 'socket'
 
 include Clockwork
 
+hostname = Socket.gethostname
+
 handler do |job|
   puts "Running #{job}"
-  system("cd /home/rahal/Documents/dev/jobs/food-scraping && bundle exec rails runner 'ScrapedFoodsController.new.scrape'")
+  system("cd /home/rahal/Documents/dev/jobs/food-scraping && bundle exec rails runner 'ScrapedFoodsController.new.scrape' -- --host=#{hostname}")
 end
+
 every(1.day, 'daily_scraper_job', at: '16:42')
-# every(1.minute, 'test_daily_scraper_job')
